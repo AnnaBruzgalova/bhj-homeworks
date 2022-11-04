2dfff1e8166496c765ac00788a21f4026a1b9b4f
Anna Bryzgalova <annabryzgalova1984@gmail.com>
2022-11-04 19:07:13 +0300

    Решение задач

---
+++ dom/book-reader/task.js
@@ -0,0 +1,62 @@
+const fontSize = Array.from(document.querySelectorAll('.font-size'));
+fontSize.forEach((item) => {
+    item.addEventListener('click', (event) => {
+        event.preventDefault();
+        let idx = fontSize.findIndex((item) => item.classList.contains('font-size_active'));
+        fontSize[idx].classList.remove('font-size_active');
+        item.classList.add('font-size_active');
+        let index = fontSize.findIndex((item) => item.classList.contains('font-size_active'));
+        const parent = item.closest('.book');
+        parent.classList.remove('book_fs-big');
+        parent.classList.remove('book_fs-small');
+        if (index === 0) {
+            parent.classList.add('book_fs-small');
+        } else if (index === 2) {
+            parent.classList.add('book_fs-big');
+        }
+    })
+})
+
+
+const colorText = Array.from(document.querySelectorAll('.color'));
+colorText.splice(3, 3);
+
+const bookContent = document.querySelector('.book__content');
+
+
+colorText.forEach((item) => {
+    item.addEventListener('click', (event) => {
+        event.preventDefault();
+        let idx = colorText.findIndex((item) => item.classList.contains('color_active'));
+        colorText[idx].classList.remove('color_active');
+        item.classList.add('color_active');
+        let index = colorText.findIndex((item) => item.classList.contains('color_active'));
+        if (index === 0) {
+            bookContent.style.color = '#000'
+        } else if (index === 1) {
+            bookContent.style.color = 'grey'
+        } else {
+            bookContent.style.color = 'whitesmoke'
+        }
+    })
+})
+
+
+const colorBg = Array.from(document.querySelectorAll('.color'));
+colorBg.splice(0, 3);
+colorBg.forEach((item) => {
+    item.addEventListener('click', (event) => {
+        event.preventDefault();
+        let idx = colorBg.findIndex((item) => item.classList.contains('color_active'));
+        colorBg[idx].classList.remove('color_active');
+        item.classList.add('color_active');
+        let index = colorBg.findIndex((item) => item.classList.contains('color_active'));
+        if (index === 0) {
+            bookContent.style.background = '#000'
+        } else if (index === 1) {
+            bookContent.style.background = 'grey'
+        } else {
+            bookContent.style.background = 'white'
+        }
+    })
+})
